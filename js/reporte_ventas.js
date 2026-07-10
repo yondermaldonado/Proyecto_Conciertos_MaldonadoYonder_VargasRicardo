53c0231f3538859fdf076025c298a7c0641edd42
Yonder <yondermaldonado23@gmail.com>
2026-07-10 15:19:15 -0500

    fix: orden del codigo

--- js/reporte_ventas.js
+++ js/reporte_ventas.js
@@ -3,7 +3,6 @@
 ========================================== */
 
 document.addEventListener("DOMContentLoaded", () => {
-
     document
         .getElementById("form-reporte")
         .addEventListener("submit", generarReporteMensual);
@@ -15,25 +14,20 @@ document.addEventListener("DOMContentLoaded", () => {
 ========================================== */
 
 function generarReporteMensual(e) {
-
     e.preventDefault();
 
     const anio = Number(document.getElementById("rep-anio").value);
     const mes = Number(document.getElementById("rep-mes").value);
-
     const ventas = StorageManager.get("sales");
-
     const reporte = {};
 
     ventas.forEach(venta => {
 
         const fecha = new Date(venta.timestamp);
-
         if (
             fecha.getFullYear() === anio &&
             (fecha.getMonth() + 1) === mes
         ) {
-
             venta.tickets.forEach(ticket => {
 
                 if (!reporte[ticket.id]) {
@@ -44,19 +38,12 @@ function generarReporteMensual(e) {
                         nombre: ticket.name,
                         cantidad: 0,
                         total: 0
-
                     };
-
                 }
-
                 reporte[ticket.id].cantidad++;
-
                 reporte[ticket.id].total += ticket.price;
-
             });
-
         }
-
     });
 
     pintarReporte(reporte);
@@ -66,57 +53,40 @@ function generarReporteMensual(e) {
 function pintarReporte(reporte) {
 
     const tbody = document.querySelector("#tabla-reporte tbody");
-
+    
     tbody.innerHTML = "";
 
     let totalEntradas = 0;
-
     let totalVentas = 0;
 
     Object.values(reporte).forEach(evento => {
 
         totalEntradas += evento.cantidad;
-
         totalVentas += evento.total;
 
         tbody.innerHTML += `
 
             <tr>
-
                 <td>${evento.codigo}</td>
-
                 <td>${evento.nombre}</td>
-
                 <td>${evento.cantidad}</td>
-
                 <td>$${evento.total.toLocaleString()}</td>
-
             </tr>
-
         `;
-
     });
 
     document.getElementById("total-general").innerHTML = `
-        
         <h3>
-
             Total entradas vendidas:
             ${totalEntradas}
-
         </h3>
-
         <h3>
-
             Total vendido:
             $${totalVentas.toLocaleString()} COP
-
         </h3>
-
     `;
 
     if (Object.keys(reporte).length === 0) {
-
         document.getElementById("mensaje-reporte").innerHTML =
             "<h3>No existen ventas para ese período.</h3>";
     } else {
